49a73a3bba044271670a232ff812e7800f709ba9
ipodishima <marian.paul2@gmail.com>
2012-03-30 09:49:25 +0200

    Initial Commit

---
+++ XML/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  XML
+//
+//  Created by Marian PAUL on 30/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ XML/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XML
+//
+//  Created by Marian PAUL on 30/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
